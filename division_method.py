b710020bc77b29a7dc0ddf393ce59cecb77d6286
Nikita <86921169+MasterNikPro@users.noreply.github.com>
2024-05-28 18:37:52 +0300

    full pascal method ready

--- division_method.py
+++ division_method.py
@@ -1,7 +1,22 @@
-#pascal test for dividing from methodichka
+# pascal test for dividing from methodichka
 
 
 def pascal(number, dividor):
-    number_string= str(number)
-    lenght_of_number=len(number_string)-1
-    remainder_array=[1]
+    number_string = str(number)
+    lenght_of_number = len(number_string)
+    # массив для залишків після ділення перший елемент 1 бо 10^0 це 1
+    remainder_array = [1]
+    for i in range(1, lenght_of_number):
+        # кожний наступний член послідовності це минуле значення помноженне на 10 и залишок від ділення dividor
+        remainder_array.append((remainder_array[i - 1] * 10) % dividor)
+    sum_string = 0
+
+    for i in range(lenght_of_number):
+        # робимо число суммою
+        sum_string += int(number_string[lenght_of_number - i - 1]) * remainder_array[i]
+        # дивимось на відповідь
+    return sum_string % dividor == 0
+
+a = pascal(10, 6)
+print(a)
+
