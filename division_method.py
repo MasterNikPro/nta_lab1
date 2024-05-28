2a461bb3cdfec69623d01baf5f57fbf70a06bffb
Nikita <86921169+MasterNikPro@users.noreply.github.com>
2024-05-28 18:45:39 +0300

    full divison method

--- division_method.py
+++ division_method.py
@@ -19,4 +19,22 @@ def pascal(number, dividor):
 
 a = pascal(10, 6)
 print(a)
+def division(value):
+    dividers=[]
+     #перевіряємо на подільність на 2
+    while value % 2 == 0:
+        dividers.append(2)
+        value //= 2
+    #перевіряємо тепер як воно ділиться на непарні числа йдемо до корня з значення +1 щоб оптимізувати алгоритм
+    for i in range(3, int(value ** 0.5) + 1, 2):
+        while pascal(value, i):
+            dividers.append(i)
+            value //= i
+    #перевірка на простоту після всіх маніпуляцій
+    if(value>2):
+        dividers.append(value)
+    return dividers
 
+n = 13195
+factors = division(n)
+print(f"Простые делители числа {n}: {factors}")
